6591de46c7c8ef9f7a1b227ae0d84811f5028782
IvanNSwart <iswart@jhb.dvt.co.za>
2022-04-18 20:46:50 +0200

    feat: :sparkles: Converting my web app to an pwa
    converting from an web app to an pwa

--- Holiday-Planner/src/environments/environment.prod.ts
+++ Holiday-Planner/src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+	production: false,
+	firebase: {
+		apiKey: "AIzaSyAOf70j8A9fW43SEU9GraflEtQWQyWffV0",
+		authDomain: "holiday-planner-d4a6b.firebaseapp.com",
+		databaseURL:
+			"https://holiday-planner-d4a6b-default-rtdb.asia-southeast1.firebasedatabase.app",
+		projectId: "holiday-planner-d4a6b",
+		storageBucket: "holiday-planner-d4a6b.appspot.com",
+		messagingSenderId: "444231628152",
+		appId: "1:444231628152:web:930b3dc1d3ce495d63c48d",
+		measurementId: "G-N3C2N7S7VQ",
+	},
 };

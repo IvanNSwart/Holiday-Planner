ecb9764bbb74a107313c5dfd34c30bd1c6ac084d
IvanNSwart <iswart@jhb.dvt.co.za>
2022-04-24 18:40:32 +0200

    feat: :bug: store
    store

--- Holiday-Planner/src/app/store/actions/planner.actions.ts
+++ Holiday-Planner/src/app/store/actions/planner.actions.ts
@@ -21,6 +21,48 @@ export const getEventsSuccess = createAction(
 	"[Holiday] getEventsSuccess",
 	props<{ events: IItineraryItem[] }>()
 );
+
+export const addEvent = createAction("[Holiday] addEvents");
+
+export const addEventSuccess = createAction(
+	"[Holiday] addEventSuccess",
+	props<{ events: IItineraryItem[] }>()
+);
+export const addTrip = createAction("[Holiday] addTrip");
+
+export const addTripSuccess = createAction(
+	"[Holiday] addTripSuccess",
+	props<{ trip: ITrip }>()
+);
+
+export const updateTrip = createAction("[Holiday] updateTrip");
+
+export const updateTripSuccess = createAction(
+	"[Holiday] updateTripSuccess",
+	props<{ trip: ITrip }>()
+);
+
+export const updateEvent = createAction("[Holiday] updateEvent");
+
+export const updateEventSuccess = createAction(
+	"[Holiday] updateEventSuccess",
+	props<{ events: IItineraryItem[] }>()
+);
+
+export const deleteEvent = createAction("[Holiday] deleteEvent");
+
+export const deleteEventSuccess = createAction(
+	"[Holiday] deleteEventSuccess",
+	props<{ events: IItineraryItem[] }>()
+);
+
+export const deleteTrip = createAction("[Holiday] deleteTrip");
+
+export const deleteTripSuccess = createAction(
+	"[Holiday] deleteTripSuccess",
+	props<{ trip: ITrip }>()
+);
+
 export const setTrip = createAction(
 	"[Holiday] setTrip",
 	props<{ trip: ITrip }>()
